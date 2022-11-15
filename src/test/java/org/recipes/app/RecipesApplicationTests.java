e473398ef2a85df8a88e659f14bab4f28b81d34f
Vinicius Sanches <v.sanches@quin.md>
2022-11-15 17:15:47 +0100

    intial structure

---
+++ src/test/java/org/recipes/app/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.recipes.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
